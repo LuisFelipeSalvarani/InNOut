52cb769abe262e9b8e67a9faeb1c2d227ef3539d
LuisFelipeSalvarani <felipe_salvarani@hotmail.com>
2024-03-23 07:30:21 -0300

    login ok

--- src/config/config.php
+++ src/config/config.php
@@ -1,7 +1,7 @@
 <?php
 
 date_default_timezone_set('America/Sao_Paulo');
-setlocale(LC_TIME, 'pt_BR.utf-8', 'pt_BR', 'portuguese');
+setlocale(LC_TIME, 'pt_br.utf-8', 'pt_br', 'portuguese');
 
 // Pastas
 define('MODEL_PATH', realpath(dirname(__FILE__)) . '/../models');
@@ -13,6 +13,8 @@ define('EXCEPTION_PATH', realpath(dirname(__FILE__)) . '/../exceptions');
 // Arquivos
 require_once(realpath(dirname(__FILE__) . '/database.php'));
 require_once(realpath(dirname(__FILE__) . '/loader.php'));
+require_once(realpath(dirname(__FILE__) . '/session.php'));
 require_once (realpath(MODEL_PATH . "/Model.php"));
+require_once (realpath(MODEL_PATH . "/User.php"));
 require_once (realpath(EXCEPTION_PATH . "/AppException.php"));
 require_once (realpath(EXCEPTION_PATH . "/ValidationException.php"));
